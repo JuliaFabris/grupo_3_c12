1151683ef48c383c87939aadcda58c0cae78b76a
Julia <yosoyjulia@gmail.com>
2021-12-10 21:04:27 -0300

    agrego slider en product-detail, su css en la carpeta y lo hago partial, su javascript en carpeta js, comento los intentos anteriores de related

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -3,8 +3,21 @@ let peliculas=require('../data/peliculas.json')
 
 let controller = {
     product: (req, res) => {
+
+        let productDetailId = +req.params.id;
+
+        let product = product.find(product => product.id === productDetailId)
+        let relatedProducts = products.filter(relatedProduct => relatedProduct.genero === product.genero)|| (relatedProduct.genero==pelicula.anio);
+       
         res.render('product-detail',{
-            Titulo:"Detalle"})
+            Titulo:"Detalle de",
+            product,
+            sliderTitle: "Productos relacionados",
+            sliderProducts: relatedProducts,
+
+
+        })
+    
     }
 }
 
