32931e5454f7ff486a9c3cd6391ecf5a7edd0bc9
Julia <yosoyjulia@gmail.com>
2021-12-14 13:29:54 -0300

    ayuda

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,13 +4,12 @@ let peliculas=require('../data/products/peliculas.json')
 let controller = {
     product: (req, res) => {
         
-        res.render("product-detail",{
-            titulo:pelicula.nombre}
-        )
+        res.render("/product-detail",{
+            titulo:"Detalle"})
    
 
 }
-
+}
 module.exports = controller
 
 
