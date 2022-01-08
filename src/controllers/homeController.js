4a6f2e1f50c6d67fe029ddacd747b11f13ae1892
Julia <yosoyjulia@gmail.com>
2022-01-08 16:08:19 -0300

    homeController corregido

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,6 +6,42 @@ let {
 } = require('../database'), {
     getListAllUniqueAtributes
 } = require('../middlewares/ourLib')
+const {products, generos} = require("../database/index")
+module.exports = {
+    inicio: (req, res) => {        
+        res.render('home', {
+            titulo: "Inicio",
+            peliculas: products,
+            generos,
+            anios: products,
+            /*favorites: tableFavorites.getFavoritesByUser(1)*/
+        })
+    },    FilterByGender: (req, res) => {        
+            let list = [],
+            id = +req.params.id;
+        let gender = tableGeneros.get(id);       
+         tablePeliculas.all.forEach(peli => {
+            let categorias = peli.category;
+            console.log(gender)
+            console.log(peli.category)
+            console.log(categorias.includes(gender.name))
+            if(categorias.includes(gender.name)) list.push(peli);
+        })
+        console.log(list.length)
+        res.send(list)
+    }
+}
+
+
+
+/*let {
+    tablePeliculas,
+    tableFavorites,
+    tableGeneros,
+    aniosEnLista
+} = require('../database'), {
+    getListAllUniqueAtributes
+} = require('../middlewares/ourLib')
 
 module.exports = {
     inicio: (req, res) => {
@@ -36,4 +72,4 @@ module.exports = {
 
         res.send(list)
     }
-}
+}*/
