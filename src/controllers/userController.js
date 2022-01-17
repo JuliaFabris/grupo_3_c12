58bd94d87699139e17afb5e9c30f41d7f0e47ca1
Julia <yosoyjulia@gmail.com>
2022-01-17 19:51:37 -0300

    aceptando cambios

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,58 +1,15 @@
 let {login, register} = require('../database')
-const { users, register } = require('../database/dataUser/index.js');
-const { validationResult } = require('express-validator')
 
 module.exports = {
     "loginPage": (req, res) => {
         res.render('login',{
-            titulo:"Log In",
-            session: req.session
-        })
+            titulo:"Log In"})
     },
+
     "login": (req, res) => {
-        let errors = validationResult(req);
-       
-        if(errors.isEmpty()){
-            let user = users.find(user => user.email === req.body.email);
-           
-            req.session.user = {
-                id: user.id,
-                name: user.name,
-                last_name: user.last_name,
-                email: user.email,
-                avatar: user.avatar,
-                rol: user.rol
-            }
-
-           if(req.body.remember){
-               const TIME_IN_MILISECONDS = 60000
-               res.cookie("userTriMovi", req.session.user, {
-                   expires: new Date(Date.now() + TIME_IN_MILISECONDS),
-                   httpOnly: true,
-                   secure: true
-               })
-           }
-
-            res.locals.user = req.session.user;
-
             res.redirect('/')
-
-        }else{
-            res.render('login', {
-                errors: errors.mapped(),
-                session: req.session
-            })
-        }
     },
-    logout: (req, res) => {
-        req.session.destroy();
-        if(req.cookies.userTriMovi){
-            res.cookie('userTriMovi', "", { maxAge: -1 })
-        }
-        res.redirect('/')
-    }, 
 
-    
     "registerPage": (req, res) => {
         res.render('register',{
             titulo:"Registro"})
--- src/middlewares/cookieSession.js
+++
@@ -1,9 +0,0 @@
-function cookieSession (req, res, next) {
-    if(req.cookies.userTriMovi){
-        req.session.user = req.cookies.userTriMovi;
-        res.locals.user = req.session.user
-    }
-    next()
-}
-
-module.exports = cookieSession
