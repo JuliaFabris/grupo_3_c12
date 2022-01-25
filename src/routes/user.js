eae04d11fa932a8a58aabf477bf72a8f49e65196
Julia <yosoyjulia@gmail.com>
2022-01-25 20:46:47 -0300

    commiteamos cambios para no crear conflictos

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,10 @@
 let router = require('express').Router()
 let controller = require('../controllers/userController')
-let checkLogin = require('../middlewares/userCheckLogin')
+let checkLogin = require('../middlewares/userAdminCheck')
 let uploadFile = require ('../middlewares/uploadAvatar')
 let registerValidator = require('../validations/registerValidator')
 let loginValidator = require ('../validations/loginValidator')
-
+let userAdminCheck= require ('../middlewares/userAdminCheck')
 
 /* GET - formulario de inicio de sesion */
 router.get('/login', controller.loginPage)
@@ -12,7 +12,7 @@ router.get('/register', controller.registerPage)
 
  /*POST */
 router.post('/register', uploadFile.single('avatarimage'),registerValidator, controller.processRegister)
-router.post('/login', loginValidator, controller.login)
+router.post('/login', loginValidator,userAdminCheck,controller.login)
 
 /* GET */
 router.get('/carrito', controller.carrito)
