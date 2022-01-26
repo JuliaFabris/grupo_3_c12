8f0919d130cfe77bf31ebdfdca08295e2113e70a
Agustina <bolcheagustina@hotmail.com>
2022-01-25 23:35:17 -0300

    cambios esteticos

--- src/validations/registerValidator.js
+++ src/validations/registerValidator.js
@@ -1,5 +1,6 @@
 const { check, body } = require('express-validator');
 const {getUsers} = require('../database')
+const bcrypt = require("bcryptjs");
 
 module.exports = [
     check('name')
@@ -15,12 +16,18 @@ module.exports = [
     .withMessage('Debes ingresar un email válido'),
 
     body('email').custom((value) => { /*Comparamos las contraseñas*/
-       let user = getUsers.find(user => user.email == value);
+       let user = getUsers.find(user=>{ 
+            return user.email == value 
+        })
 
-        return (user!=undefined? false : true);
+        if(user){
+            return false
+        }else{
+            return true
+        }
     }).withMessage('Email registrado'),
 
-    check('pass1')
+    check('pass')
     .notEmpty()
     .withMessage('Debes escribir tu contraseña')
     .isLength({
@@ -28,13 +35,10 @@ module.exports = [
         max: 12
     })
     .withMessage('La contraseña debe tener entre 6 y 12 caracteres'),
-    
-    check('pass2').custom((value, {req}) => value !== req.body.pass1? false : true)
-    .withMessage('La contraseña debe tener entre 6 y 12 caracteres'),
-
-    body('pass2').custom((value, {req}) => value !== req.body.pass ? false : true)
+    body('pass2').custom((value, {req}) => value !== req.body.pass1 ? false : true)
     .withMessage('Las contraseñas no coinciden'),
-    
+
+
     check('terms')
     .isString('on')
     .withMessage('Debes aceptar las bases y condiciones')
