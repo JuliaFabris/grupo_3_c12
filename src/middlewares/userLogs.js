ac29f10d92ee1033fed78a27e722f541f6f1970e
Julia <yosoyjulia@gmail.com>
2022-01-27 14:52:47 -0300

    userLogs.js corregido

--- src/middlewares/userLogs.js
+++ src/middlewares/userLogs.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const path = require ('path');
+
 
 function userLogs(req, res, next){
-    fs.appendFileSync('src/logs/userLogs.txt', `El usuario ingresó a la ruta: ${req.url}\n`)
+    fs.appendFileSync(path.join(__dirname,'../database/userLogs.txt', `El usuario ingresó a la ruta: ${req.url}\n`))
     next()
 } 
 
