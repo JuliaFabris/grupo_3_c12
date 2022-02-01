9e146e505fdc94b483bad6965abe634aedb751e8
Julia <yosoyjulia@gmail.com>
2022-02-01 10:26:42 -0300

    commit

---
+++ adminController.js
@@ -0,0 +1,138 @@
+let { products, writeProductsJSON, categories } = require('../database/dataBase')
+let fs = require('fs')
+let subcategories = products.map(product => product.subcategory)
+let uniqueSubcategories = subcategories.filter((x, i, a) => a.indexOf(x) == i)
+let { validationResult } = require('express-validator')
+
+const db = require('../database/models');
+
+const Products = db.Product;
+const Categories = db.Category;
+const Subcategories = db.Subcategory;
+
+
+let controller = {
+    index: (req, res) => {
+        res.render('admin/adminIndex', {
+            session: req.session
+        })
+    },
+    products: (req, res) => {
+        res.render('admin/products/adminProducts', {
+            products,
+            session: req.session
+        })
+    },
+    create: (req, res) => {
+       res.render('admin/adminProductCreateForm', {
+           categories,
+           subcategories: uniqueSubcategories,
+           session: req.session
+       })
+    },
+    store: (req, res) => {
+        let errors = validationResult(req)
+
+        if (errors.isEmpty()) {
+            let lastId = 1;
+
+            products.forEach(product => {
+                if(product.id > lastId){
+                    lastId = product.id
+                }
+            });
+    
+            const {name, price, category, subcategory, description, discount} = req.body
+    
+            let newProduct = {
+                id: lastId + 1,
+                name: name.trim(),
+                price: +price.trim(),
+                category: +category,
+                subcategory,
+                description: description.trim(),
+                discount: +discount,
+                image: req.file ? [req.file.filename] : ["default-image.png"]
+            }
+    
+            /* let newProduct = {
+                ...req.body,
+                id: lastId + 1,
+                image: req.file ? req.file.filename : "default-image.png"
+            } */
+    
+            products.push(newProduct)
+    
+            writeProductsJSON(products)
+    
+            res.redirect('/admin/products')
+        } else {
+            res.render('admin/products/adminProductCreateForm', {
+                categories,
+                subcategories: uniqueSubcategories,
+                errors: errors.mapped(),
+                old: req.body,
+                session: req.session
+
+            })
+        }
+    },
+    edit: (req, res) => {
+        let productId = +req.params.id;
+        
+        let product = products.find(product => product.id === productId)
+        res.render('admin/products/adminProductEditForm', {
+            product,
+            categories, 
+            subcategories: uniqueSubcategories,
+            session: req.session
+        })
+    },
+    update: (req, res) => {
+        let productId = +req.params.id;
+
+        const {name, price, category, subcategory, description, discount} = req.body
+
+        products.forEach(product => {
+            if(product.id === productId){
+                product.id = product.id,
+                product.name = name.trim(),
+                product.price = +price.trim(),
+                product.category = +category,
+                product.subcategory = subcategory,
+                product.description = description.trim(),
+                product.discount = +discount,
+                product.image = req.file ? [req.file.filename] : product.image
+            }
+        })
+
+        writeProductsJSON(products)
+
+        res.redirect('/admin/products')
+    },
+    destroy: (req, res) => {
+        let productId = +req.params.id;
+
+		products.forEach(product => {
+			if(product.id === productId){
+				if(fs.existsSync("./public/images/productos/", product.image[0])){
+					fs.unlinkSync(`./public/images/productos/${product.image[0]}`)
+				}else{
+					console.log('No encontré el archivo')
+				}
+
+				let productToDestroyIndex = products.indexOf(product) // si lo encuentra devuelve el indice si no -1
+				if(productToDestroyIndex !== -1) {
+					products.splice(productToDestroyIndex, 1)
+				}else{  // primer parámetro es el indice del elemento a borrar, el segundo, la cantidad a eliminar 
+					console.log('No encontré el producto')
+				}
+			}
+		})
+
+		writeProductsJSON(products)
+		res.redirect('/admin/products')
+    }
+}
+
+module.exports = controller
---
+++ usersController.js
@@ -0,0 +1,102 @@
+//const { users, writeUsersJSON } = require('../database/dataBase');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const db = require('../database/models');
+
+const Users = db.User;
+
+let controller = {
+    login: (req, res) => {
+        res.render('login', {
+            session: req.session
+        })
+    },
+    processLogin: (req, res) => {
+        let errors = validationResult(req);
+       
+        if(errors.isEmpty()){
+            Users.findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+            .then(user => {
+                req.session.user = {
+                    id: user.id,
+                    name: user.name,
+                    last_name: user.last_name,
+                    email: user.email,
+                    avatar: user.avatar,
+                    rol: user.rol
+                }
+    
+               if(req.body.remember){
+                   const TIME_IN_MILISECONDS = 60000
+                   res.cookie("userArtisticaDali", req.session.user, {
+                       expires: new Date(Date.now() + TIME_IN_MILISECONDS),
+                       httpOnly: true,
+                       secure: true
+                   })
+               }
+    
+                res.locals.user = req.session.user;
+    
+                res.redirect('/')
+            })
+        }else{
+            res.render('login', {
+                errors: errors.mapped(),
+                session: req.session
+            })
+        }
+    },
+    register: (req, res) => {
+        res.render('register', {
+            session: req.session
+        })
+    },
+    processRegister: (req, res) => {
+        let errors = validationResult(req);
+       
+        if(errors.isEmpty()){
+            let { name, last_name, email, pass1 } = req.body;
+            Users.create({
+                name, 
+                last_name,
+                email,
+                pass: bcrypt.hashSync(pass1, 10),
+                avatar: req.file ? req.file.filename : 'default-image.png',
+                rol: 0
+            })
+            .then(() => {
+                res.redirect('/users/login')
+            })
+        }else{
+            res.render('register', {
+                errors: errors.mapped(),
+                old: req.body,
+                session: req.session
+            })
+        }
+    },
+    logout: (req, res) => {
+        req.session.destroy();
+        if(req.cookies.userArtisticaDali){
+            res.cookie('userArtisticaDali', "", { maxAge: -1 })
+        }
+        res.redirect('/')
+    }, 
+    profile: (req, res) => {
+        Users.findByPk(req.session.user.id, {
+            include: [{association: 'addresses'}]
+        })
+        .then((user) => {
+            res.render('userProfile', {
+                user, 
+                session: req.session
+            })
+        })
+    }
+}
+
+module.exports = controller
