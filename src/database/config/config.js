a9fb96b1a6b0dc3357fe511146022cf3b802b8d9
Edgar-Rios <sistunla2020@gmail.com>
2022-02-20 20:43:31 -0300

    estructura db

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     "username": "root",
     "password": null,
     "database": "trimovie-grupo3",
-    "host": "127.0.0.1",
+    "host": "localhost",
     "dialect": "mysql"
   },
   "test": {
--- src/database/models/Movie.js
+++ src/database/models/Movie.js
@@ -8,6 +8,39 @@ module.exports = (sequelize, dataTypes) => {//se empieza con module.exports para
             autoIncrement: true,
         },
        
+        title: {
+            type: dataTypes.STRING(500),
+            allowNull: false
+        },
+        director_id: {
+            type: dataTypes.INTEGER(50).UNSIGNED,
+            allowNull: false
+        },
+
+        year: {
+            type: dataTypes.INTEGER,
+            allowNull: false
+        },
+        image: {
+            type: dataTypes.STRING(50),
+            allowNull: false
+        },
+        trailer: {
+            type: dataTypes.STRING(52),
+            allowNull: false,
+        },
+        length: {
+            type: dataTypes.INTEGER(50).UNSIGNED,
+            allowNull: false
+        },
+        sinopsis: {
+            type: dataTypes.TEXT,
+            allowNull: false
+        },
+        price: {
+            type: dataTypes.INTEGER(10).UNSIGNED,
+            allowNull: false
+        },
     };
     
     let config = {
@@ -22,29 +55,29 @@ module.exports = (sequelize, dataTypes) => {//se empieza con module.exports para
 
     //Aquí debes realizar lo necesario para crear las relaciones con los otros modelos (Genre - Actor)
 
-    // Movie.associate = function(models) {
+    Movie.associate = function(models) {
 
-        // Movie.belongsTo(models.Director,{
-        //     as: "director",
-        //     foreignKey:"director_id",
-        // }) 
+        Movie.belongsTo(models.Director,{
+            as: "director",
+            foreignKey:"director_id",
+        }) 
 
-        // Movie.belongsToMany(models.Genre, {
-        //     as: "genres", /* le pertenece a uno o más generos */
-        //     through: "movie_has_genre",
-        //     foreignKey: "movie_id",
-        //     otherKey: "genre_id", 
-        // })
+        Movie.belongsToMany(models.Genre, {
+            as: "genres", /* le pertenece a uno o más generos */
+            through: "movie_has_genre",
+            foreignKey: "movie_id",
+            otherKey: "genre_id", 
+        })
 
-        // Movie.belongsToMany(models.Actor, { 
-        //     as: "actors",
-        //     through: "actor_has_movie", /* tabla pivot */
-        //     foreignKey: "movie_id",
-        //     otherKey: "actor_id", /* es la otra llave */
-        //     timestamps: false /* si queremos que se guarde el timestamps tenemos que configurar el createdAt y el updatedAt */
-        // })
+        Movie.belongsToMany(models.Actor, { 
+            as: "actors",
+            through: "actor_has_movie", /* tabla pivot */
+            foreignKey: "movie_id",
+            otherKey: "actor_id", /* es la otra llave */
+            timestamps: false /* si queremos que se guarde el timestamps tenemos que configurar el createdAt y el updatedAt */
+        })
 
-    // }
+    }
 
     return Movie;
 };
